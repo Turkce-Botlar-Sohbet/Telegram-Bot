2f6e167ca23aa7b768355dd15ad0d2bfa01875a0
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2021-05-14 13:10:44 +0300

    Update admin.js

--- handlers/middlewares/admin.js
+++ handlers/middlewares/admin.js
@@ -2,20 +2,20 @@
 
 
 const Admins = (ctx, next) => {
+	if (ctx.chat && ['private', 'group', 'supergroup'].includes(ctx.chat.type)) {
 
-    if (ctx.chat.type !== 'channel') {
-        if (ctx.chat.id > 0) return next();
+		if (ctx.chat.id > 0) return next();
 
-        return ctx.telegram.getChatAdministrators(ctx.chat.id)
-            .then((data) => {
-                if (!data || !data.length) return;
-                ctx.chat._admins = data;
-                ctx.from._is_in_admin_list = data.some(adm => adm.user.id === ctx.from.id);
-            })
-            .catch(console.log)
-            .then(_ => next(ctx));
+		return ctx.telegram.getChatAdministrators(ctx.chat.id)
+			.then((data) => {
+				if (!data || !data.length) return;
+				ctx.chat._admins = data;
+				ctx.from._is_in_admin_list = data.some(adm => adm.user.id === ctx.from.id);
+			})
+			.catch(console.log)
+			.then(_ => next(ctx));
+     }
 
-    }
 };
 
 module.exports = Admins;
