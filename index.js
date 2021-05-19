a0ac57b551b1f7fd2d5a3f237c96ba4e7fd34ee2
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2021-05-20 01:40:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,17 +9,35 @@ const bot       = new Telegraf(BOT_API);
 
 const config = require('./config');
 
+// Bota start verdiğinizde atılan ilk mesaj
 bot.start((ctx) => {
     return ctx.reply("Hey");
 });
 
+
 bot.hears(/selam/ig, async (ctx, next) => {
-        ctx.telegram.sendPhoto(ctx.chat.id, 'https://www.ajanskirim.com/wp-content/uploads/2019/03/1525995635Merhaba.jpg', { caption:  `<b>${ctx.from.first_name}</b>`,  parse_mode: 'HTML' })
+    await ctx.telegram.sendPhoto(ctx.chat.id,
+        'https://www.ajanskirim.com/wp-content/uploads/2019/03/1525995635Merhaba.jpg',
+        { caption:  `<b>${ctx.from.first_name}</b>`,  parse_mode: 'HTML' })
     return next();
 });
 
+
+bot.command('botsohbet', async (ctx, next) => {
+    
+    await bot.telegram.sendDocument(ctx.chat.id, {
+        source: './dosyalar/botsohbet.mp4'
+    }, {
+        filename: 'botsohbet.mp4',
+        caption: 'https://t.me/botsohbet'
+    })
+    return next()
+    
+})
+
+
 bot.command('komut', async (ctx, next) => {
-    ctx.telegram.sendMessage(ctx.chat.id, `<b>${ctx.from.first_name}</b>`, { parse_mode: 'HTML' })
+    await ctx.telegram.sendMessage(ctx.chat.id, `<b>${ctx.from.first_name}</b>`, { parse_mode: 'HTML' })
     return next();
 });
 
@@ -29,19 +47,31 @@ bot.use(
     require('./plugin')
 );
 
-
-bot.telegram.getMe().then(me => {
-    console.log(`Bot Başlatıldı! => ${me.username}`);
-});
-
-
+// Kodlarda hata çıkarsa bunun sayesinde çalışmaya devam eder.
 bot.catch((err) => {
     console.log('Error: ', err)
 })
 
-bot.launch({
-  webhook: {
-    domain: `${URL}`,
-    port: `${PORT}`
-  }
+// Botun kullanıcı adını alan bir kod.
+bot.telegram.getMe().then(botInfo => {
+    bot.options.username = botInfo.username
+    console.log(`Bot Başlatıldı! => ${bot.options.username}`)
 })
+
+// Heroku sitesinde botunuzun kullanıcı adı gözükür -> deneyselbot.herokuapp.com
+const cb = function(req, res) {
+    res.end(`${bot.options.username}`)
+}
+
+// Botun webhook ile çalışmasını sağlar.
+bot.launch({
+    webhook: {
+        domain: `${URL}`,
+        port: `${PORT}`,
+        cb
+    }
+})
+
+// Bu botumuzu nazikçe durdurmayı etkinleştirir.
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
